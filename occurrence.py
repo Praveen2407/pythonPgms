625f6a0a5157566558de6363435d4cef9ab727fc
Praveen2407 <73053989+Praveen2407@users.noreply.github.com>
2021-09-15 07:56:21 +0530

    Create occurrence.py
    Simple program to find char in string

---
+++ occurrence.py
@@ -0,0 +1,10 @@
+# Count occurrences of a character in string
+
+str = "MakeinIndia"
+count = 0
+  
+for x in str:
+    if x == 'i':
+        count = count + 1
+# printing result 
+print ("Count of i in Given string is : " , count)
